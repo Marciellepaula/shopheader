18ffe8147a663df00a803b01b98a7d76803cd866
Marcielle Paula <marcielle0644@gmail.com>
2025-03-22 13:03:31 -0300

    criando componemye card

---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss'
+})
+export class ProductCardComponent {
+  @Input() product!: { image: string; name: string };
+}
+
+
+
