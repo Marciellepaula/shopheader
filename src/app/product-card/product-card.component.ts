059a42a1a5e2cba2284d887db0b98ff915ecd0e7
Marcielle Paula <marcielle0644@gmail.com>
2025-03-22 19:06:50 -0300

    ajustes

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -12,3 +12,13 @@ export class ProductCardComponent {
 
 
 
+// import { Component, Input } from '@angular/core';
+
+// @Component({
+//   selector: 'app-product-card',
+//   templateUrl: './product-card.component.html',
+//   styleUrls: ['./product-card.component.css']
+// })
+// export class ProductCardComponent {
+//   @Input() product: any; // Receiving product data from parent
+// }
